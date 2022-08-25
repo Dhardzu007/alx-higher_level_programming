3c32b7641ba09d562a5ec653b47cfbd852ede91f
Rashmi Dayanand <dayanandrashmi2@gmail.com>
2022-08-25 13:13:00 -0700

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    sum_arg = 0
-    for i in sys.argv[1:]:
-        sum_arg += int(i)
-        print("{:d}".format(sum_arg))
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
