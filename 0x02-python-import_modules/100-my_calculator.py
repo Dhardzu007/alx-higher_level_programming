5899fbc46ad79bcd621a63f07a88c441ff927209
Rashmi Dayanand <dayanandrashmi2@gmail.com>
2022-08-25 13:35:28 -0700

    please please please

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,26 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
-    nargs = len(sys.argv) - 1
-    if nargs != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
-    op = sys.argv[2]
-     if op != '+' and op != '-' and op != '*' and op != '/':
-         print("Unknown operator. Available operators: +, -, * and /")
-         sys.exit(1)
-
     from calculator_1 import add, sub, mul, div
-    a = int(sys.argv[1])
-     b = int(sys.argv[3])
-
-    if op == '+':
-        print("{} + {} = {}".format(a, b, add(a, b)))
-     elif op == '-':
-         print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif op == '*':
-        print("{} * {} = {}".format(a, b, mul(a, b)))
-    else:
-        print("{} / {} = {}".format(a, b, div(a, b)))
+    num_arg = len(sys.argv) - 1
+    if num_arg < 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a, operator, b = sys.argv[1:]
+    if operator not in "+-*/":
+          print("Unknown operator. Available operators: +, -, * and /")
+          exit(1)
+    print("{:s} {:s} {:s} = ".format(a, operator, b), end="")
+    for ope, func in [('+', add), ('-', sub), ('*', mul), ('/', div)]:
+        if operator == ope:
+             print("{:d}".format(func(int(a), int(b))))
