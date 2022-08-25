c01a7e33146a51c2a9ec01cc0ef8415f62a49a45
Rashmi Dayanand <dayanandrashmi2@gmail.com>
2022-08-25 12:46:20 -0700

    1-calculation.py

---
+++ 0x02-python-import_modules/additional_files/add_0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Addition function
+
+    Args:
+    a: first int
+    b: second int
+
+    Return:
+        The return value. a + b
+    """
+
+    return (a + b)
